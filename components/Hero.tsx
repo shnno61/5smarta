34ae09860603990c2b5d5584916d224c86f9da4d
ahmed elshnnawy <ahmedelshnawy91@gmail.com>
2024-07-30 18:41:11 +0300

    hero shift 3

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = ({ title, description, imageSrc, imageAlt }: HeroProps) => (
     <div className="absolute inset-0 bg-black opacity-40 z-0"></div>
     <div className="relative z-10 flex flex-1 flex-col order-1 xl:order-1 xl:w-1/2 items-center xl:items-start">
       <div className="px-4 py-8 mx-auto max-w-screen-lg xl:max-w-full 2xl:px-16 2xl:py-12 3xl:px-24 3xl:py-16 4xl:px-32 4xl:py-20">
-        <h1 className="text-3xl mt-20 text-yellow-500 font-extrabold leading-tight md:text-4xl lg:text-5xl lg:leading-tight mb-4 drop-shadow-md text-center xl:text-left 2xl:text-6xl 3xl:text-7xl 4xl:text-8xl">
+        <h1 className="text-3xl mt-10 text-yellow-500 font-extrabold leading-tight md:text-4xl lg:text-5xl lg:leading-tight mb-4 drop-shadow-md text-center xl:text-left 2xl:text-6xl 3xl:text-7xl 4xl:text-8xl">
           {title}
         </h1>
         <p className="text-base md:text-lg lg:text-xl leading-relaxed mb-8 drop-shadow-sm text-center xl:text-left 2xl:text-2xl 3xl:text-3xl 4xl:text-4xl">
@@ -20,27 +20,31 @@ const Hero = ({ title, description, imageSrc, imageAlt }: HeroProps) => (
         </p>
       </div>
       <div className="relative rounded-3xl mb-8 xl:hidden w-full max-w-md md:max-w-lg lg:max-w-md xl:mx-2 2xl:mx-4 3xl:mx-6 4xl:mx-8">
-        <Image
-          src={imageSrc}
-          alt={imageAlt}
-          width={800}
-          height={600}
-          layout="responsive"
-          className="rounded-lg shadow-lg transition-transform transform hover:scale-105"
-          priority
-        />
+        <div className="pt-4"> {/* Adjust padding as needed */}
+          <Image
+            src={imageSrc}
+            alt={imageAlt}
+            width={2048}
+            height={1365}  
+            layout="responsive"
+            className="rounded-lg shadow-lg transition-transform transform hover:scale-105"
+            priority
+          />
+        </div>
       </div>
     </div>
     <div className="relative w-full order-2 xl:order-2 xl:w-1/2 hidden xl:block xl:ml-8 2xl:ml-12 3xl:mx-16 4xl:mx-20">
-      <div className="relative w-full h-0 pb-[75%]">
-        <Image
-          src={imageSrc}
-          alt={imageAlt}
-          layout="fill"
-          objectFit="cover"
-          className="rounded-lg shadow-lg transition-transform transform hover:scale-105"
-          priority
-        />
+      <div className="relative pt-4"> {/* Adjust padding as needed */}
+        <div className="relative w-full h-0 pb-[66.67%]"> {/* Aspect ratio container */}
+          <Image
+            src={imageSrc}
+            alt={imageAlt}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-lg shadow-lg transition-transform transform hover:scale-105"
+            priority
+          />
+        </div>
       </div>
     </div>
   </section>
